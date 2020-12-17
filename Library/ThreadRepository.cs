5f62e798a6eee6e750306b39fecc3f5f30705169
Richard Gustavsson <retg94@gmail.com>
2020-12-17 14:40:16 +0100

    Added main menu
    Added show threads and posts and logout

---
+++ Library/ThreadRepository.cs
@@ -0,0 +1,31 @@
+﻿using Dapper;
+using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Library
+{
+    public static class ThreadRepository
+    {
+        private const string _connectionString = "Data Source=.\\forum2.0.db";
+
+        public static List<Thread> GetThreads()
+        {
+            using (var connection = new SqliteConnection(_connectionString))
+            {
+                var threads = connection.Query<Thread>("SELECT * FROM thread");
+                return threads.ToList();
+            }
+        }
+        public static Thread GetThreadById(int id)
+        {
+            using (var connection = new SqliteConnection(_connectionString))
+            {
+                var thread = connection.QueryFirst<Thread>($"SELECT * FROM thread WHERE thread_id={id}");
+                return thread;
+            }
+        }
+    }
+}
